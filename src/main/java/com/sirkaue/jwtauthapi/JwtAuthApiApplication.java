84bcf6cce35fb806594e331ec4f0ba7e6d09e59a
sirkaue <kauekdslopes@gmail.com>
2025-07-09 21:33:27 -0300

    Initial commit

---
+++ src/main/java/com/sirkaue/jwtauthapi/JwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.sirkaue.jwtauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthApiApplication.class, args);
+    }
+
+}
