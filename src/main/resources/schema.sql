10806b9d2cac7736424fb2bb9c68f5ecc26a59d2
sirkaue <kauekdslopes@gmail.com>
2025-07-10 19:57:15 -0300

    feat: criar tabela tb_user

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS tb_user;
+
+CREATE TABLE tb_user
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name     VARCHAR(100) NOT NULL,
+    email    VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
