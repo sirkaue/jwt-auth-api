bf7b1f3e20f463e2d6a76768124c83425802b068
sirkaue <kauekdslopes@gmail.com>
2025-07-25 23:23:06 -0300

    feat: adicionar serviço para buscar usuário por ID

---
+++ src/main/java/com/sirkaue/jwtauthapi/domain/user/service/UserService.java
@@ -0,0 +1,18 @@
+package com.sirkaue.jwtauthapi.domain.user.service;
+
+import com.sirkaue.jwtauthapi.domain.user.model.User;
+import com.sirkaue.jwtauthapi.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public User getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
