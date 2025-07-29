ee1eeaa75118c87ca00e2e49fd628da6122092c4
sirkaue <kauekdslopes@gmail.com>
2025-07-29 17:11:17 -0300

    feat: implementar UserDetails com UserAuthenticated

---
+++ src/main/java/com/sirkaue/jwtauthapi/security/authetication/UserAuthenticated.java
@@ -0,0 +1,34 @@
+package com.sirkaue.jwtauthapi.security.authetication;
+
+import com.sirkaue.jwtauthapi.domain.user.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserAuthenticated implements UserDetails {
+
+    private final User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
