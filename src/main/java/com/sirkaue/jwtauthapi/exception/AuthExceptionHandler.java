c2b3ce1b695876a7dfba4ccfcc25d59edc12d54c
sirkaue <kauekdslopes@gmail.com>
2025-07-29 17:42:46 -0300

    feat: adicionar tratamento para exceções de JWT expirado ou inválido

--- src/main/java/com/sirkaue/jwtauthapi/exception/AuthExceptionHandler.java
+++ src/main/java/com/sirkaue/jwtauthapi/exception/AuthExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.sirkaue.jwtauthapi.exception;
 
+import com.sirkaue.jwtauthapi.domain.user.exception.JwtExpiredException;
+import com.sirkaue.jwtauthapi.domain.user.exception.JwtInvalidException;
 import com.sirkaue.jwtauthapi.domain.user.exception.UserNotFoundException;
 import com.sirkaue.jwtauthapi.dto.response.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +24,12 @@ public class AuthExceptionHandler {
     @ExceptionHandler({BadCredentialsException.class, UsernameNotFoundException.class})
     public ResponseEntity<ErrorResponse> handleAuthErrors(HttpServletRequest request) {
         ErrorResponse error = new ErrorResponse(request, HttpStatus.UNAUTHORIZED, new Exception("Invalid email or password"));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    @ExceptionHandler({JwtExpiredException.class, JwtInvalidException.class})
+    public ResponseEntity<ErrorResponse> handleJwtExceptions(RuntimeException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(request, HttpStatus.UNAUTHORIZED, ex);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
 }
