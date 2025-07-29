cb69f1a27f4ef4462e9ea70fa5e29678d43715c7
sirkaue <kauekdslopes@gmail.com>
2025-07-29 17:08:10 -0300

    refactor: lançar exceção específica ao buscar usuário por ID

--- src/main/java/com/sirkaue/jwtauthapi/domain/user/service/UserService.java
+++ src/main/java/com/sirkaue/jwtauthapi/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.sirkaue.jwtauthapi.domain.user.service;
 
+import com.sirkaue.jwtauthapi.domain.user.exception.UserNotFoundException;
 import com.sirkaue.jwtauthapi.domain.user.model.User;
 import com.sirkaue.jwtauthapi.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,6 @@ public class UserService {
 
     public User getById(Long id) {
         return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
