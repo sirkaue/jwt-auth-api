78685fc54ba08c0ec7e45197488f8cdaabef1698
sirkaue <kauekdslopes@gmail.com>
2025-08-04 19:43:14 -0300

    refactor: ajustar entidade para refresh token e remover anotação builder

--- src/main/java/com/sirkaue/jwtauthapi/domain/user/model/User.java
+++ src/main/java/com/sirkaue/jwtauthapi/domain/user/model/User.java
@@ -3,13 +3,14 @@ package com.sirkaue.jwtauthapi.domain.user.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
+
 @Entity
 @Table(name = "tb_user")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
 
     @Id
@@ -21,5 +22,14 @@ public class User {
     @Column(unique = true)
     private String email;
 
-    private String password;
+    private String passwordHash;
+
+    private String refreshToken;
+    private Instant refreshTokenExpiresAt;
+
+    public User(String name, String email, String passwordHash) {
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
 }
